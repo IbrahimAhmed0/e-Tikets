51bc33db6be2dcbc5f43f59680a2c32c3e5c10bd
Anas <anasabualzahab@hotmail.com>
2022-08-26 10:43:20 +0300

    .

---
+++ e-Tikets/Data/Services/CinemaService.cs
@@ -0,0 +1,55 @@
+﻿using e_Tikets.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace e_Tikets.Data.Services
+{
+    public class CinemaService : ICinemaService
+    {
+
+        private readonly AppDbContext _appDbContext;
+        public CinemaService(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<IEnumerable<Cienme>> GetAllasyncCinema()
+        {
+            var result = await _appDbContext.Cienmes.ToListAsync();
+            return result;
+        }
+
+        public async Task<Cienme> GetByIdAsyncCinema(int id)
+        {
+            var result = await _appDbContext.Cienmes.FirstOrDefaultAsync(c => c.Id == id);
+            return result;
+        }
+
+        public async Task AddAsyncCinema(Cienme cinema)
+        {
+            await _appDbContext.Cienmes.AddAsync(cinema);
+            await _appDbContext.SaveChangesAsync();
+
+            
+        }
+
+        public async Task<Cienme> UpdateAsyncCinema(int id, Cienme newCinema)
+        {
+            _appDbContext.Cienmes.Update(newCinema);
+            await _appDbContext.SaveChangesAsync();
+            return newCinema;
+
+        }
+
+        public async Task DeleteAsyncCinema(int id)
+        {
+            var removedCinema = await _appDbContext.Cienmes.FirstOrDefaultAsync(c=> c.Id == id);
+             _appDbContext.Remove(removedCinema);
+            await _appDbContext.SaveChangesAsync();
+
+        }
+
+
+    }
+}
---
+++ e-Tikets/Data/Services/ICinemaService.cs
@@ -0,0 +1,18 @@
+﻿using e_Tikets.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace e_Tikets.Data.Services
+{
+    public interface ICinemaService
+    {
+
+            Task<IEnumerable<Cienme>> GetAllasyncCinema();
+            Task<Cienme> GetByIdAsyncCinema(int id);
+            Task AddAsyncCinema(Cienme cinema);
+            Task<Cienme> UpdateAsyncCinema(int id, Cienme newCinema);
+            Task DeleteAsyncCinema(int id);
+
+
+    }
+}
