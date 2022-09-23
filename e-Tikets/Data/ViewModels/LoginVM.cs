e1ea4e7ca56392ede3f095a373886eed090c9fe6
Ibrahim AlShiekh Ahmed <ibrahimsamer184@gmail.com>
2022-09-23 10:25:19 +0300

    update-atuhincated-role-manager

---
+++ e-Tikets/Data/ViewModels/LoginVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace e_Tikets.Data.ViewModels
+{
+    public class LoginVM
+    {
+
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "Email Address is required!")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }  
+    }
+}
